4059fdd868e6707da6f6485bcb0bddaa200c0687
Kiryanov D V <dmitriyk@aethernet.io>
2025-01-21 18:13:15 +0300

    Updating registrator.

--- examples/registrator/registrator.cpp
+++ examples/registrator/registrator.cpp
@@ -26,6 +26,47 @@
 #include "aether/tele/tele.h"
 
 
+namespace ae {
+#if defined AE_DISTILLATION
+static Aether::ptr CreateAetherInstrument(Domain& domain) {
+  Aether::ptr aether = domain.CreateObj<ae::Aether>(GlobalId::kAether);
+
+
+  RegisterWifiAdapter::ptr adapter = domain.CreateObj<RegisterWifiAdapter>(
+      GlobalId::kEsp32WiFiAdapter, aether, aether->poller,
+      std::string(WIFI_SSID), std::string(WIFI_PASS));
+  adapter.SetFlags(ae::ObjFlags::kUnloadedByDefault);
+  adapter->Connect();
+
+  aether->adapter_factories.emplace_back(std::move(adapter));
+
+#  if AE_SUPPORT_REGISTRATION
+  // localhost
+  aether->registration_cloud->AddServerSettings(IpAddressPortProtocol{
+      {IpAddress{IpAddress::Version::kIpV4, {127, 0, 0, 1}}, 9010},
+      Protocol::kTcp});
+  // cloud address
+  aether->registration_cloud->AddServerSettings(IpAddressPortProtocol{
+      {IpAddress{IpAddress::Version::kIpV4, {35, 224, 1, 127}}, 9010},
+      Protocol::kTcp});
+  // cloud name address
+  aether->registration_cloud->AddServerSettings(
+      NameAddress{"registration.aethernet.io", 9010, Protocol::kTcp});
+#  endif  // AE_SUPPORT_REGISTRATION
+  return aether;
+}
+#endif
+
+static Aether::ptr LoadAether(Domain& domain) {
+  Aether::ptr aether;
+  aether.SetId(GlobalId::kAether);
+  domain.LoadRoot(aether);
+  assert(aether);
+  return aether;
+}
+
+}  // namespace ae
+
 void AetherRegistrator(const std::string &ini_file);
 
 void AetherRegistrator(const std::string &ini_file) {
@@ -50,4 +91,27 @@ void AetherRegistrator(const std::string &ini_file) {
 
   AE_TELED_DEBUG("Sodium key={}", sodium_key);
   AE_TELED_DEBUG("Hydrogen key={}", hydrogen_key);
+
+    auto fs =
+      ae::FileSystemHeaderFacility{};
+
+#ifdef AE_DISTILLATION
+  // create objects in instrument mode
+  {
+    ae::Domain domain{ae::ClockType::now(), fs};
+    fs.remove_all();
+    ae::Aether::ptr aether = ae::CreateAetherInstrument(domain);
+#  if AE_SIGNATURE == AE_ED25519
+    aether->crypto->signs_pk_[ae::SignatureMethod::kEd25519] =
+        ae::SodiumSignPublicKey{
+            ae::MakeLiteralArray(sodium_key)};
+
+#  elif AE_SIGNATURE == AE_HYDRO_SIGNATURE
+    aether->crypto->signs_pk_[ae::SignatureMethod::kHydroSignature] =
+        ae::HydrogenSignPublicKey{
+            ae::MakeLiteralArray(hydrogen_key)};
+#  endif  // AE_SIGNATURE == AE_ED25519
+    domain.SaveRoot(aether);
+  }
+#endif  // AE_DISTILLATION
 }
