78d5b5ca8adce9518b3e0041d6d166ed631a3d8e
BartolomeyKant <notevill@gmail.com>
2025-03-27 14:37:05 +0500

    make socket packet queue manager an action

--- aether/transport/low_level/tcp/socket_packet_queue_manager.h
+++ aether/transport/low_level/tcp/socket_packet_queue_manager.h
@@ -21,59 +21,79 @@
 #include <mutex>
 #include <type_traits>
 
+#include "aether/common.h"
 #include "aether/actions/action_view.h"
 #include "aether/actions/action_list.h"
+#include "aether/events/multi_subscription.h"
 
 #include "aether/transport/actions/packet_send_action.h"
 #include "aether/transport/low_level/tcp/socket_packet_send_action.h"
 
 namespace ae {
 template <typename TSocketPacketSendAction,
-          std::enable_if_t<std::is_base_of_v<SocketPacketSendAction,
-                                             TSocketPacketSendAction>,
-                           int> = 0>
-class SocketPacketQueueManager {
+          AE_REQUIRERS((std::is_base_of<SocketPacketSendAction,
+                                        TSocketPacketSendAction>))>
+class SocketPacketQueueManager
+    : Action<SocketPacketQueueManager<TSocketPacketSendAction>> {
  public:
-  SocketPacketQueueManager() = default;
+  using BaseAction = Action<SocketPacketQueueManager<TSocketPacketSendAction>>;
+
+  explicit SocketPacketQueueManager(ActionContext action_context)
+      : BaseAction{std::move(action_context)} {}
+
+  AE_CLASS_NO_COPY_MOVE(SocketPacketQueueManager)
+
+  TimePoint Update(TimePoint current_time) override {
+    Send();
+    return current_time;
+  }
 
   ActionView<SocketPacketSendAction> AddPacket(
       TSocketPacketSendAction&& packet_send_action) {
     auto lock = std::unique_lock{queue_lock_};
     auto view = actions_.Add(std::move(packet_send_action));
     queue_.emplace(view);
-    if (current_ == nullptr) {
-      lock.unlock();
-      Send();
-    }
+    on_sent_subs_.Push(
+        view->SubscribeOnResult([this](auto const&) { Send(); }));
+    BaseAction::Trigger();
     return view;
   }
 
-  // Triggers send on queued action
+  /**
+   * \brief Triggers send on queued action.
+   * This invoked automatically in every Update or, if requires, immediately on
+   * socket write event (\see IPoller)
+   */
   void Send() {
     auto lock = std::lock_guard{queue_lock_};
     while (!queue_.empty()) {
-      current_ = &queue_.front();
-      if (*current_) {
-        if (((*current_)->state() == PacketSendAction::State::kQueued) ||
-            ((*current_)->state() == PacketSendAction::State::kProgress)) {
-          (*current_)->Send();
-        }
-        if ((*current_)->state() == PacketSendAction::State::kProgress) {
-          break;
-        }
+      auto action = queue_.front();
+      if (!action) {
+        queue_.pop();
+        continue;
       }
-      current_ = nullptr;
+      if (auto state = action->state().get();
+          (state == PacketSendAction::State::kQueued) ||
+          (state == PacketSendAction::State::kProgress)) {
+        action->Send();
+      }
+      if (action->state() == PacketSendAction::State::kProgress) {
+        break;
+      }
       queue_.pop();
     }
   }
 
-  bool empty() const { return queue_.empty(); }
+  bool empty() const {
+    auto lock = std::lock_guard{queue_lock_};
+    return queue_.empty();
+  }
 
  private:
-  std::mutex queue_lock_;
+  mutable std::mutex queue_lock_;
   ActionStore<TSocketPacketSendAction> actions_;
   std::queue<ActionView<TSocketPacketSendAction>> queue_;
-  ActionView<TSocketPacketSendAction>* current_ = nullptr;
+  MultiSubscription on_sent_subs_;
 };
 }  // namespace ae
 
