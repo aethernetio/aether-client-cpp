b06d0920e6384cd00af7509c63789c1d67ac4dda
BartolomeyKant <notevill@gmail.com>
2025-05-07 12:19:20 +0500

    use new Uid::From* functions

--- examples/benches/send_message_delays/main.cpp
+++ examples/benches/send_message_delays/main.cpp
@@ -83,9 +83,9 @@ class TestSendMessageDelaysAction : public Action<TestSendMessageDelaysAction> {
 #if AE_SUPPORT_REGISTRATION
     aether_->clients().clear();
     auto reg_sender = aether_->RegisterClient(
-        Uid{MakeLiteralArray("3ac931653d37497087a6fa4ee27744e4")});
+        Uid::FromLit("3ac93165-3d37-4970-87a6-fa4ee27744e4"));
     auto reg_receiver = aether_->RegisterClient(
-        Uid{MakeLiteralArray("3ac931653d37497087a6fa4ee27744e4")});
+        Uid::FromLit("3ac93165-3d37-4970-87a6-fa4ee27744e4"));
 
     registration_subscriptions_.Push(
         reg_sender->ResultEvent().Subscribe([this](auto const& action) {
