bfc6905f7a8efae1f3a901c856e96070ac80e10b
BartolomeyKant <notevill@gmail.com>
2025-05-07 12:19:03 +0500

    add FromString and FromLit functions for uid

--- aether/uid.h
+++ aether/uid.h
@@ -19,15 +19,35 @@
 
 #include <array>
 #include <cstdint>
+#include <cstddef>
 
+#include "aether/literal_array.h"
 #include "aether/reflect/reflect.h"
 
 namespace ae {
-
-struct Uid;
 struct Uid {
   static constexpr std::size_t kSize = 16;
+  static constexpr std::size_t kUidStringSize = 32 + 4;
   static const Uid kAether;
+
+  /**
+   * \brief Get UID from string.
+   * The https://www.ietf.org/rfc/rfc4122.txt format recognized only.
+   */
+  static Uid FromString(std::string_view str);
+
+  template <std::size_t Size>
+  static constexpr Uid FromLit(char const (&str)[Size]) {
+    static_assert((Size - 1) == kUidStringSize, "Wrong uid format");
+
+    // parse string like "f81d4fae-7dec-11d0-a765-00a0c91e6bf6"
+    return Uid{
+        ConcatArrays(FillArray<8>(str, std::make_index_sequence<4>()),
+                     FillArray<4>(str + 9, std::make_index_sequence<2>()),
+                     FillArray<4>(str + 14, std::make_index_sequence<2>()),
+                     FillArray<4>(str + 19, std::make_index_sequence<2>()),
+                     FillArray<12>(str + 24, std::make_index_sequence<6>()))};
+  }
 
   explicit constexpr Uid(std::array<std::uint8_t, kSize> uid) : value(uid) {}
   Uid() = default;
