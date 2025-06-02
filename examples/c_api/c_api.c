03d2c3c9b7e5d818bac8051d2e5fafae079dea3e
Kiryanov D V <dmitriyk@aethernet.io>
2025-06-02 18:01:05 +0300

    Updating C api example.

---
+++ examples/c_api/c_api.c
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2025 Aethernet Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "aether_capi.h"
+
+static const char* kWifiSsid = "Test1234";
+static const char* kWifiPass = "Test1234";
+
+int AetherCApiExample();
+
+int AetherCApiExample() {
+  /**
+   * Construct a main aether application class.
+   * It's include a Domain and Aether instances accessible by getter methods.
+   * It has Update, WaitUntil, Exit, IsExit, ExitCode methods to integrate it in
+   * your update loop.
+   * Also it has action context protocol implementation \see Action.
+   * To configure its creation \see AetherAppConstructor.
+   */
+  
+  AetherClassHandle* obj = AetherClassCreate();  
+  AetherClassDestroy(obj);
+    
+  return 0;
+}
