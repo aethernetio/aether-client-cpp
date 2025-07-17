e126daa3fb7583a1bcefbac6bdac57244c4d45e8
BartolomeyKant <notevill@gmail.com>
2025-07-17 17:43:31 +0500

    add repeatable task

---
+++ aether/actions/repeatable_task.cpp
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2025 Aethernet Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "aether/actions/repeatable_task.h"
+
+namespace ae {
+RepeatableTask::RepeatableTask(ActionContext action_context, Task&& task,
+                               Duration interval, int max_repeat_count)
+    : Action{action_context},
+      task_{std::move(task)},
+      interval_{interval},
+      max_repeat_count_{max_repeat_count},
+      state_{State::kRun},
+      current_repeat_{} {}
+
+ActionResult RepeatableTask::Update(TimePoint current_time) {
+  if (state_.changed()) {
+    switch (state_.Acquire()) {
+      case State::kRun:
+        Run(current_time);
+        break;
+      case State::kWait:
+        break;
+      case State::kStop:
+        return ActionResult::Stop();
+      case State::kRepeatCountExceeded:
+        return ActionResult::Error();
+    }
+  }
+
+  if (state_ == State::kWait) {
+    return CheckInterval(current_time);
+  }
+  return {};
+}
+
+void RepeatableTask::Stop() {
+  state_ = State::kStop;
+  Action::Trigger();
+}
+
+void RepeatableTask::Run(TimePoint current_time) {
+  if ((max_repeat_count_ != kRepeatCountInfinite) &&
+      (current_repeat_ > max_repeat_count_)) {
+    state_ = State::kRepeatCountExceeded;
+    Action::Trigger();
+    return;
+  }
+  current_repeat_++;
+  last_execution_time_ = current_time;
+  // execute task
+  task_();
+}
+
+ActionResult RepeatableTask::CheckInterval(TimePoint current_time) {
+  auto next_time = last_execution_time_ + interval_;
+  if (next_time >= current_time) {
+    // repeat run
+    state_ = State::kRun;
+    Action::Trigger();
+    return {};
+  }
+  return ActionResult::Delay(next_time);
+}
+
+}  // namespace ae
