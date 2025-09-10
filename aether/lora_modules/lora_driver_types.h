9a9892253c2c0e46ac2dbac853c0ced137a28da9
Kiryanov D V <dmitriyk@aethernet.io>
2025-09-10 17:52:05 +0300

    Lora`s modules object factory.

---
+++ aether/lora_modules/lora_driver_types.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2025 Aethernet Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef AETHER_LORA_MODULES_LORA_DRIVER_TYPES_H_
+#define AETHER_LORA_MODULES_LORA_DRIVER_TYPES_H_
+
+#include "aether/reflect/reflect.h"
+#include "aether/serial_ports/serial_port_types.h"
+
+namespace ae {
+  
+  struct LoraInit {
+  AE_REFLECT_MEMBERS(serial_init)
+  SerialInit serial_init;
+};
+
+}  // namespace ae
+
+#endif  // AETHER_LORA_MODULES_LORA_DRIVER_TYPES_H_
---
+++ aether/lora_modules/lora_module_factory.h
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2025 Aethernet Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef AETHER_LORA_MODULES_LORA_MODULE_FACTORY_H_
+#define AETHER_LORA_MODULES_LORA_MODULE_FACTORY_H_
+
+#include <memory>
+
+#include "aether/lora_modules/ilora_driver.h"
+
+#define AE_LORA_MODULE_EBYTE22_ENABLED 1
+
+// check if any mode is enabled
+#if (AE_LORA_MODULE_EBYTE22_ENABLED == 1)
+#  define AE_LORA_MODULE_ENABLED 1
+
+namespace ae {
+class LoraModuleDriverFactory {
+ public:
+  static ILoraDriver::ptr CreateLoraModule(LoraInit lora_init, Domain* domain);
+};
+}  // namespace ae
+
+#endif
+
+#endif  // AETHER_LORA_MODULES_LORA_MODULE_FACTORY_H_
