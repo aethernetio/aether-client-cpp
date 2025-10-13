89fd7464c77a43de41b9e408d81de838d5012285
BartolomeyKant <notevill@gmail.com>
2025-10-13 21:29:36 +0500

    add Dima's impl for esp32

--- aether/serial_ports/serial_port_factory.cpp
+++ aether/serial_ports/serial_port_factory.cpp
@@ -31,6 +31,8 @@ std::unique_ptr<ISerialPort> SerialPortFactory::CreatePort(
   return std::make_unique<WINSerialPort>(action_context, serial_init, poller);
 #elif ESP32_SERIAL_PORT_ENABLED == 1
   return std::make_unique<ESP32SerialPort>(action_context, serial_init, poller);
+  на return std::make_unique<Esp32SerialPort>(action_context, serial_init,
+                                              poller);
 #elif UNIX_SERIAL_PORT_ENABLED == 1
   return std::make_unique<UnixSerialPort>(action_context, serial_init, poller);
 #else
