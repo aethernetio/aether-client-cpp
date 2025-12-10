ab678a93d460b5eea88ab7bb7417f04240c50de5
Kiryanov D V <dmitriyk@aethernet.io>
2025-12-10 16:20:32 +0300

    Adding gateway Lora  device.

---
+++ aether/lora_modules/gw_lora_device.h
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2025 Aethernet Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef AETHER_LORA_MODULES_GW_LORA_DEVICE_H_
+#define AETHER_LORA_MODULES_GW_LORA_DEVICE_H_
+
+#include <cstdint>
+
+#include "aether/types/server_id.h"
+#include "aether/types/client_id.h"
+#include "aether/gateway_api/gateway_api.h"
+#include "aether/transport/gateway/gateway_device.h"
+
+namespace ae {
+using DeviceId = std::uint8_t;
+
+class GwLoraDevice final : public IGatewayDevice {
+ public:
+  explicit GwLoraDevice(ActionContext action_context);
+  ~GwLoraDevice() override;
+
+  ActionPtr<StreamWriteAction> ToServer(ClientId client_id, ServerId server_id,
+                                        DataBuffer&& data) override;
+
+  ActionPtr<StreamWriteAction> ToServer(ClientId client_id,
+                                        ServerEndpoints const& server_endpoints,
+                                        DataBuffer&& data) override;
+
+  FromServerEvent::Subscriber from_server_event() override;
+
+ private:
+  ActionContext action_context_;
+  DeviceId device_id_;
+
+  ProtocolContext protocol_context_;
+  GatewayApi gateway_api_;
+  GatewayClientApi gateway_client_api_;
+};
+}  // namespace ae
+
+#endif  // AETHER_LORA_MODULES_GW_LORA_DEVICE_H_
