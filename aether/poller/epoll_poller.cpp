b4be70aba4fc5f2e9f1f07132f3491f90491bb04
BartolomeyKant <notevill@gmail.com>
2026-01-07 17:53:54 +0500

    add new poller interface and unix pollers

--- aether/poller/epoll_poller.cpp
+++ aether/poller/epoll_poller.cpp
@@ -21,207 +21,190 @@
 #  include <fcntl.h>
 #  include <unistd.h>
 #  include <sys/epoll.h>
+#  include <sys/eventfd.h>
 
 #  include <array>
-#  include <mutex>
-#  include <atomic>
-#  include <thread>
 #  include <cerrno>
 #  include <cstring>
-
-#  include "aether/events/events.h"
 
 #  include "aether/poller/poller_tele.h"
 
 namespace ae {
+namespace epoll_poller_internal {
+std::uint32_t PollerEventsToEpol(EventType event) {
+  std::uint32_t event_flags = 0;
+  if ((event & EventType::kRead) != 0) {
+    event_flags |= EPOLLIN;
+  }
+  if ((event & EventType::kWrite) != 0) {
+    event_flags |= EPOLLOUT;
+  }
+  if ((event & EventType::kError) != 0) {
+    event_flags |= EPOLLRDHUP | EPOLLPRI | EPOLLERR | EPOLLHUP;
+  }
+  return event_flags;
+}
 
-inline constexpr auto kMaxEvents = 10;
+EventType EpollEventToEventType(std::uint32_t event_flags) {
+  EventType event{};
+  if ((event_flags & EPOLLIN) != 0) {
+    event |= EventType::kRead;
+  }
+  if ((event_flags & EPOLLOUT) != 0) {
+    event |= EventType::kWrite;
+  }
+  if ((event_flags & (EPOLLRDHUP | EPOLLPRI | EPOLLERR | EPOLLHUP)) != 0) {
+    event |= EventType::kError;
+  }
+  return static_cast<EventType>(event);
+}
 
-class EpollPoller::PollWorker {
-  static constexpr auto kPipeReadEnd = 0;
-  static constexpr auto kPipeWriteEnd = 1;
+}  // namespace epoll_poller_internal
 
- public:
-  PollWorker()
-      : wake_up_pipe_{WakeUpPipe()},
-        epoll_fd_{InitEpoll()},
-        thread_(&PollWorker::Loop, this) {
-    AE_TELE_INFO(kEpollWorkerCreate);
-    // add wake up pipe to epoll
-    wake_up_subscription_ =
-        Add(DescriptorType{wake_up_pipe_[kPipeReadEnd]})
-            .Subscribe(MethodPtr<&PollWorker::EmptyWakeUpPipe>{this});
+EpollImpl::EpollImpl()
+    : epoll_fd_{InitEpoll()},
+      event_fd_{MakeEventFd()},
+      thread_(&EpollImpl::Loop, this) {
+  AE_TELE_INFO(kEpollWorkerCreate);
+
+  // add wake up fd to epoll
+  if (event_fd_ != -1) {
+    Event(DescriptorType{event_fd_}, EventType::kRead, [](auto) {});
+  }
+}
+
+EpollImpl::~EpollImpl() {
+  AE_TELED_DEBUG("Destroy EpollImpl event fd {}, thread is joinable {}",
+                 event_fd_, thread_.joinable());
+
+  stop_requested_ = true;
+  if (event_fd_ != -1) {
+    // write something
+    std::uint64_t b{1};
+    auto res = write(event_fd_, &b, sizeof(b));
+    if (res != sizeof(b)) {
+      AE_TELED_ERROR("Failed to write to event fd {} {}", errno,
+                     strerror(errno));
+    }
   }
 
-  ~PollWorker() {
-    AE_TELED_DEBUG("Destroy PollWorker WRITE PIPE {}, thread is joinable {}",
-                   wake_up_pipe_[kPipeWriteEnd], thread_.joinable());
-
-    stop_requested_ = true;
-    if (wake_up_pipe_[kPipeWriteEnd] != -1) {
-      // write something
-      [[maybe_unused]] auto res = write(wake_up_pipe_[kPipeWriteEnd], "", 1);
-      close(wake_up_pipe_[kPipeWriteEnd]);
-    }
-
-    if (thread_.joinable()) {
-      thread_.join();
-    }
-
-    if (epoll_fd_ != -1) {
-      close(epoll_fd_);
-    }
-    if (wake_up_pipe_[kPipeReadEnd] != -1) {
-      close(wake_up_pipe_[kPipeReadEnd]);
-    }
-    AE_TELE_INFO(kEpollWorkerDestroyed);
+  if (thread_.joinable()) {
+    thread_.join();
   }
 
-  [[nodiscard]] EpollPoller::OnPollEventSubscriber Add(
-      DescriptorType descriptor) {
-    AE_TELE_DEBUG(kEpollAddDescriptor, "Add poller descriptor {}", descriptor);
-    struct epoll_event epoll_event;
-    // watch in and out
-    epoll_event.events = EPOLLIN | EPOLLOUT;
-    // watch only edge triggered events
-    epoll_event.events |= EPOLLET;
-    // watch if socket closed
-    epoll_event.events |= EPOLLRDHUP | EPOLLPRI | EPOLLERR | EPOLLHUP;
-    epoll_event.data.fd = descriptor;
+  if (epoll_fd_ != -1) {
+    close(epoll_fd_);
+  }
+  if (event_fd_ != -1) {
+    close(event_fd_);
+  }
+  AE_TELE_INFO(kEpollWorkerDestroyed);
+}
 
-    auto res = epoll_ctl(epoll_fd_, EPOLL_CTL_ADD, descriptor, &epoll_event);
-    if (res < 0) {
-      AE_TELE_ERROR(kEpollAddFailed, "Failed to add to epoll {} {}", errno,
-                    strerror(errno));
+void EpollImpl::Event(DescriptorType fd, EventType event, EventCb cb) {
+  AE_TELE_DEBUG(kEpollAddDescriptor, "Poller event fd:{} event:{}", fd, event);
+  struct epoll_event epoll_event;
+  epoll_event.events = epoll_poller_internal::PollerEventsToEpol(event);
+  // watch only edge triggered events
+  epoll_event.events |= EPOLLET;
+  epoll_event.data.fd = fd;
+
+  auto lock = std::scoped_lock{poller_mutex_};
+  auto [_, new_event] = event_map_.insert_or_assign(fd, std::move(cb));
+  int op = new_event ? EPOLL_CTL_ADD : EPOLL_CTL_MOD;
+  auto res = epoll_ctl(epoll_fd_, op, fd, &epoll_event);
+  if (res < 0) {
+    AE_TELE_ERROR(kEpollAddFailed, "Failed to add to epoll {} {}", errno,
+                  strerror(errno));
+    assert(false);
+  }
+}
+
+void EpollImpl::Remove(DescriptorType fd) {
+  AE_TELE_DEBUG(kEpollRemoveDescriptor, "Remove poller event {}", fd);
+
+  auto lock = std::scoped_lock{poller_mutex_};
+  event_map_.erase(fd);
+
+  struct epoll_event epoll_event{};
+  auto res = epoll_ctl(epoll_fd_, EPOLL_CTL_DEL, fd, &epoll_event);
+  if (res < 0) {
+    if (errno != ENOENT) {
+      AE_TELE_ERROR(kEpollRemoveFailed, "Failed to remove from epoll {} {}",
+                    errno, strerror(errno));
       assert(false);
     }
-
-    return EpollPoller::OnPollEventSubscriber{poll_event_};
   }
+}
 
-  void Remove(DescriptorType descriptor) {
-    AE_TELE_DEBUG(kEpollRemoveDescriptor, "Remove poller event {}", descriptor);
-    struct epoll_event epoll_event{};
+int EpollImpl::MakeEventFd() {
+  auto fd = eventfd(0, EFD_CLOEXEC);
+  // create pipe to wake up the thread
+  if (fd < 0) {
+    AE_TELE_ERROR(kEpollCreateWakeUpFailed,
+                  "Failed to create wake up pipe {} {}", errno,
+                  strerror(errno));
+    assert(false);
+    return -1;
+  }
+  return fd;
+}
 
-    auto res = epoll_ctl(epoll_fd_, EPOLL_CTL_DEL, descriptor, &epoll_event);
+int EpollImpl::InitEpoll() {
+  auto fd = epoll_create1(EPOLL_CLOEXEC);
+  if (fd < 0) {
+    AE_TELE_ERROR(kEpollInitFailed, "Failed to create epoll fd {} {}", errno,
+                  strerror(errno));
+    assert(false);
+  }
+  return fd;
+}
+
+void EpollImpl::Loop() {
+  static constexpr auto kMaxEvents = 10;
+  std::array<struct epoll_event, kMaxEvents> events;
+
+  while (!stop_requested_) {
+    auto res = epoll_wait(epoll_fd_, events.data(), events.size(), -1);
     if (res < 0) {
-      if (errno != ENOENT) {
-        AE_TELE_ERROR(kEpollRemoveFailed, "Failed to remove from epoll {} {}",
-                      errno, strerror(errno));
-        assert(false);
-      }
-    }
-  }
-
- private:
-  static std::vector<EventType> FromEpollEvent(std::uint32_t events) {
-    auto res = std::vector<EventType>{};
-    res.reserve(3);
-    if ((events & EPOLLIN) != 0) {
-      res.push_back(EventType::kRead);
-    }
-    if ((events & EPOLLOUT) != 0) {
-      res.push_back(EventType::kWrite);
-    }
-    if ((events & (EPOLLRDHUP | EPOLLPRI | EPOLLERR | EPOLLHUP)) != 0) {
-      res.push_back(EventType::kError);
-    }
-    return res;
-  }
-
-  static std::array<int, 2> WakeUpPipe() {
-    std::array<int, 2> pipes;
-    auto res = pipe2(pipes.data(), O_CLOEXEC);
-    // create pipe to wake up the thread
-    if (res < 0) {
-      AE_TELE_ERROR(kEpollCreateWakeUpFailed,
-                    "Failed to create wake up pipe {} {}", errno,
-                    strerror(errno));
-      assert(false);
-      return {-1, -1};
-    }
-    return pipes;
-  }
-
-  void EmptyWakeUpPipe(PollerEvent event) {
-    if (event.descriptor != wake_up_pipe_[kPipeReadEnd]) {
-      return;
-    }
-    // drain the pipe
-    std::array<char, 1> buf;
-    [[maybe_unused]] auto res = read(event.descriptor, buf.data(), buf.size());
-  }
-
-  static int InitEpoll() {
-    auto fd = epoll_create1(EPOLL_CLOEXEC);
-    if (fd < 0) {
-      AE_TELE_ERROR(kEpollInitFailed, "Failed to create epoll fd {} {}", errno,
-                    strerror(errno));
-      assert(false);
-    }
-    return fd;
-  }
-
-  void Loop() {
-    std::array<struct epoll_event, kMaxEvents> events;
-
-    while (!stop_requested_) {
-      auto res = epoll_wait(epoll_fd_, events.data(), events.size(), -1);
-      if (res < 0) {
-        if (errno == EINTR) {
-          continue;
-        }
-        AE_TELE_ERROR(kEpollWaitFailed, "Failed to epoll_wait {} {}", errno,
-                      strerror(errno));
-        assert(false);
+      if (errno == EINTR) {
         continue;
       }
+      AE_TELE_ERROR(kEpollWaitFailed, "Failed to epoll_wait {} {}", errno,
+                    strerror(errno));
+      assert(false);
+      continue;
+    }
 
-      for (std::size_t i = 0;
-           (i < static_cast<std::size_t>(res)) && (i < kMaxEvents); ++i) {
-        auto& event = events[i];
-        auto fd = event.data.fd;
-        for (auto ev_type : FromEpollEvent(event.events)) {
-          poll_event_.Emit(PollerEvent{fd, ev_type});
-        }
+    auto lock = std::scoped_lock{poller_mutex_};
+
+    for (std::size_t i = 0;
+         (i < static_cast<std::size_t>(res)) && (i < kMaxEvents); ++i) {
+      auto& event = events[i];
+      auto fd = event.data.fd;
+      if (fd == event_fd_) {
+        continue;
       }
+      auto poller_event = event_map_.find(fd);
+      if (poller_event == event_map_.end()) {
+        continue;
+      }
+      auto ev_type = epoll_poller_internal::EpollEventToEventType(event.events);
+      poller_event->second(ev_type);
     }
   }
-
-  std::atomic_bool stop_requested_{false};
-  std::array<int, 2> wake_up_pipe_;
-  int epoll_fd_;
-
-  EpollPoller::OnPollEvent poll_event_;
-  OnPollEventSubscriber::Subscription wake_up_subscription_;
-
-  std::thread thread_;
-};
+}
 
 EpollPoller::EpollPoller() = default;
 
-#  if defined AE_DISTILLATION
 EpollPoller::EpollPoller(Domain* domain) : IPoller(domain) {}
-#  endif
 
-EpollPoller::~EpollPoller() = default;
-
-EpollPoller::OnPollEventSubscriber EpollPoller::Add(DescriptorType descriptor) {
-  if (!poll_worker_) {
-    InitPollWorker();
+NativePoller* EpollPoller::Native() {
+  if (!impl_) {
+    impl_.emplace();
   }
-  return poll_worker_->Add(descriptor);
+  return static_cast<NativePoller*>(&*impl_);
 }
 
-void EpollPoller::Remove(DescriptorType descriptor) {
-  if (!poll_worker_) {
-    return;
-  }
-  poll_worker_->Remove(descriptor);
-}
-
-void EpollPoller::InitPollWorker() {
-  poll_worker_ = std::make_unique<PollWorker>();
-}
 }  // namespace ae
 #endif
