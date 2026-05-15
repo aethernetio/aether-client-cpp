347a3bbc3b82c594371dd4469145ead591b1c632
BartolomeyKant <notevill@gmail.com>
2026-05-15 17:23:36 +0500

    make bg95 migration

--- aether/modems/bg95_at_modem.h
+++ aether/modems/bg95_at_modem.h
@@ -18,184 +18,206 @@
 #define AETHER_MODEMS_BG95_AT_MODEM_H_
 
 #include "aether/config.h"
-
 #if AE_SUPPORT_MODEMS && AE_ENABLE_BG95
-
-#  include <chrono>
+#  include <set>
 #  include <memory>
 
-#  include "aether/modems/imodem_driver.h"
-#  include "aether/adapters/modem_adapter.h"
+#  include "aether/ae_context.h"
+#  include "aether/poller/poller.h"
+#  include "aether/actions/action_pool.h"
+#  include "aether/actions/actions_queue.h"
+#  include "aether/actions/repeatable_task.h"
 #  include "aether/serial_ports/iserial_port.h"
 #  include "aether/serial_ports/at_support/at_support.h"
 
+#  include "aether/modems/imodem_driver.h"
+
 namespace ae {
+class Bg95AtModem;
 
-static const std::map<kBaudRate, std::string> baud_rate_commands_bg95 = {
-    {kBaudRate::kBaudRate9600, "AT+IPR=9600"},
-    {kBaudRate::kBaudRate19200, "AT+IPR=19200"},
-    {kBaudRate::kBaudRate38400, "AT+IPR=38400"},
-    {kBaudRate::kBaudRate57600, "AT+IPR=57600"},
-    {kBaudRate::kBaudRate115200, "AT+IPR=115200"},
-    {kBaudRate::kBaudRate230400, "AT+IPR=230400"},
-    {kBaudRate::kBaudRate921600, "AT+IPR=921600"},
-    {kBaudRate::kBaudRate2900000, "AT+IPR=2900000"},
-    {kBaudRate::kBaudRate3000000, "AT+IPR=3000000"},
-    {kBaudRate::kBaudRate3200000, "AT+IPR=3200000"},
-    {kBaudRate::kBaudRate3684000, "AT+IPR=3684000"},
-    {kBaudRate::kBaudRate4000000, "AT+IPR=4000000"}};
-
-static const std::map<kModemBand, std::string> set_band_power_bg95 = {
-    {kModemBand::kWCDMA_B1, "AT+QNVW=539,0,\"9B9EA0{}A3A5A6\""},
-    {kModemBand::kWCDMA_B2, "AT+QNVW=1183,0,\"9C9EA0{}A3A5A6\""},
-    {kModemBand::kWCDMA_B4, "AT+QNVW=4035,0,\"9C9EA0{}A3A5A6\""},
-    {kModemBand::kWCDMA_B5, "AT+QNVW=1863,0,\"9C9EA0{}A3A5A6\""},
-    {kModemBand::kWCDMA_B8, "AT+QNVW=3690,0,\"9C9EA0{}A3A5A6\""},
-    {kModemBand::kLTE_B1, "AT+QNVFW=\"/nv/item_files/rfnv/00020992\",0100{}00"},
-    {kModemBand::kLTE_B2, "AT+QNVFW=\"/nv/item_files/rfnv/00020993\",0100{}00"},
-    {kModemBand::kLTE_B3, "AT+QNVFW=\"/nv/item_files/rfnv/00020994\",0100{}00"},
-    {kModemBand::kLTE_B4, "AT+QNVFW=\"/nv/item_files/rfnv/00020995\",0100{}00"},
-    {kModemBand::kLTE_B5, "AT+QNVFW=\"/nv/item_files/rfnv/00020996\",0100{}00"},
-    {kModemBand::kLTE_B7, "AT+QNVFW=\"/nv/item_files/rfnv/00020997\",0100{}00"},
-    {kModemBand::kLTE_B8, "AT+QNVFW=\"/nv/item_files/rfnv/00020998\",0100{}00"},
-    {kModemBand::kLTE_B12,
-     "AT+QNVFW=\"/nv/item_files/rfnv/00022191\",0100{}00"},
-    {kModemBand::kLTE_B13,
-     "AT+QNVFW=\"/nv/item_files/rfnv/00021000\",0100{}00"},
-    {kModemBand::kLTE_B17,
-     "AT+QNVFW=\"/nv/item_files/rfnv/00021001\",0100{}00"},
-    {kModemBand::kLTE_B18,
-     "AT+QNVFW=\"/nv/item_files/rfnv/00021002\",0100{}00"},
-    {kModemBand::kLTE_B19,
-     "AT+QNVFW=\"/nv/item_files/rfnv/00023133\",0100{}00"},
-    {kModemBand::kLTE_B20,
-     "AT+QNVFW=\"/nv/item_files/rfnv/00021003\",0100{}00"},
-    {kModemBand::kLTE_B25,
-     "AT+QNVFW=\"/nv/item_files/rfnv/00022351\",0100{}00"},
-    {kModemBand::kLTE_B26,
-     "AT+QNVFW=\"/nv/item_files/rfnv/00024674\",0100{}00"},
-    {kModemBand::kLTE_B28,
-     "AT+QNVFW=\"/nv/item_files/rfnv/00025477\",0100{}00"},
-    {kModemBand::kLTE_B38,
-     "AT+QNVFW=\"/nv/item_files/rfnv/00021004\",0100{}00"},
-    {kModemBand::kLTE_B39,
-     "AT+QNVFW=\"/nv/item_files/rfnv/00023620\",0100{}00"},
-    {kModemBand::kLTE_B40,
-     "AT+QNVFW=\"/nv/item_files/rfnv/00021005\",0100{}00"},
-    {kModemBand::kLTE_B41,
-     "AT+QNVFW=\"/nv/item_files/rfnv/00021670\",0100{}00"},
-    {kModemBand::kTDS_B34,
-     "AT+QNVFW=\"/nv/item_files/rfnv/00022622\",0100{}00"},
-    {kModemBand::kTDS_B39,
-     "AT+QNVFW=\"/nv/item_files/rfnv/00022663\",0100{}00"},
-    {kModemBand::kGSM_850,
-     "AT+QNVFW=\"/nv/item_files/rfnv/"
-     "00025076\",C2018A0252031A04E204AA0572063A070208CA0892095A0A220BEA0B{}{}"},
-    {kModemBand::kGSM_900,
-     "AT+QNVFW=\"/nv/item_files/rfnv/"
-     "00025077\",C2018A0252031A04E204AA0572063A070208CA0892095A0A220BEA0B{}{}"},
-    {kModemBand::kGSM_1800,
-     "AT+QNVFW=\"/nv/item_files/rfnv/"
-     "00025078\",000096005E012602EE02B6037E0446050E06D6069E0766082E09F609BE0A{"
-     "}"},
-    {kModemBand::kGSM_1900,
-     "AT+QNVFW=\"/nv/item_files/rfnv/"
-     "00025079\",000096005E012602EE02B6037E0446050E06D6069E0766082E09F609BE0A{"
-     "}"},
-    {kModemBand::kTDSCDMA_B34, "AT+QNVFW=\"/nv/item_files/rfnv/00022622\",{}"},
-    {kModemBand::kTDSCDMA_B39, "AT+QNVFW=\"/nv/item_files/rfnv/00022663\",{}"}};
-
-static const std::map<kModemBand, std::string> get_band_power_bg95 = {
-    {kModemBand::kWCDMA_B1, "AT+QNVR=539,0"},
-    {kModemBand::kWCDMA_B2, "AT+QNVR=1183,0"},
-    {kModemBand::kWCDMA_B4, "AT+QNVR=4035,0"},
-    {kModemBand::kWCDMA_B5, "AT+QNVR=1863,0"},
-    {kModemBand::kWCDMA_B8, "AT+QNVR=3690,0"},
-    {kModemBand::kLTE_B1, "AT+QNVFR=\"/nv/item_files/rfnv/00020992\",0100"},
-    {kModemBand::kLTE_B2, "AT+QNVFR=\"/nv/item_files/rfnv/00020993\",0100"},
-    {kModemBand::kLTE_B3, "AT+QNVFR=\"/nv/item_files/rfnv/00020994\",0100"},
-    {kModemBand::kLTE_B4, "AT+QNVFR=\"/nv/item_files/rfnv/00020995\",0100"},
-    {kModemBand::kLTE_B5, "AT+QNVFR=\"/nv/item_files/rfnv/00020996\",0100"},
-    {kModemBand::kLTE_B7, "AT+QNVFR=\"/nv/item_files/rfnv/00020997\",0100"},
-    {kModemBand::kLTE_B8, "AT+QNVFR=\"/nv/item_files/rfnv/00020998\",0100"},
-    {kModemBand::kLTE_B12, "AT+QNVFR=\"/nv/item_files/rfnv/00022191\",0100"},
-    {kModemBand::kLTE_B13, "AT+QNVFR=\"/nv/item_files/rfnv/00021000\",0100"},
-    {kModemBand::kLTE_B17, "AT+QNVFR=\"/nv/item_files/rfnv/00021001\",0100"},
-    {kModemBand::kLTE_B18, "AT+QNVFR=\"/nv/item_files/rfnv/00021002\",0100"},
-    {kModemBand::kLTE_B19, "AT+QNVFR=\"/nv/item_files/rfnv/00023133\",0100"},
-    {kModemBand::kLTE_B20, "AT+QNVFR=\"/nv/item_files/rfnv/00021003\",0100"},
-    {kModemBand::kLTE_B25, "AT+QNVFR=\"/nv/item_files/rfnv/00022351\",0100"},
-    {kModemBand::kLTE_B26, "AT+QNVFR=\"/nv/item_files/rfnv/00024674\",0100"},
-    {kModemBand::kLTE_B28, "AT+QNVFR=\"/nv/item_files/rfnv/00025477\",0100"},
-    {kModemBand::kLTE_B38, "AT+QNVFR=\"/nv/item_files/rfnv/00021004\",0100"},
-    {kModemBand::kLTE_B39, "AT+QNVFR=\"/nv/item_files/rfnv/00023620\",0100"},
-    {kModemBand::kLTE_B40, "AT+QNVFR=\"/nv/item_files/rfnv/00021005\",0100"},
-    {kModemBand::kLTE_B41, "AT+QNVFR=\"/nv/item_files/rfnv/00021670\",0100"},
-    {kModemBand::kTDS_B34, "AT+QNVFR=\"/nv/item_files/rfnv/00022622\",0100"},
-    {kModemBand::kTDS_B39, "AT+QNVFR=\"/nv/item_files/rfnv/00022663\",0100"},
-    {kModemBand::kGSM_850, "AT+QNVFR=\"/nv/item_files/rfnv/00025076\""},
-    {kModemBand::kGSM_900, "AT+QNVFR=\"/nv/item_files/rfnv/00025077\""},
-    {kModemBand::kGSM_1800, "AT+QNVFR=\"/nv/item_files/rfnv/00025078\""},
-    {kModemBand::kGSM_1900, "AT+QNVFR=\"/nv/item_files/rfnv/00025079\""},
-    {kModemBand::kTDSCDMA_B34, "AT+QNVFR=\"/nv/item_files/rfnv/00022622\""},
-    {kModemBand::kTDSCDMA_B39, "AT+QNVFR=\"/nv/item_files/rfnv/00022663\""}};
-
-struct Bg95Connection {
-  AE_REFLECT_MEMBERS(handle, protocol, host, port)
-  std::int32_t handle;
-  ae::Protocol protocol;
-  std::string host;
-  std::uint16_t port;
-};
-
-class Bg95AtModem final : public IModemDriver {
-  AE_OBJECT(Bg95AtModem, IModemDriver, 0)
-
- protected:
-  Bg95AtModem() = default;
-
+namespace bg95_modem_internal {
+class OpenNetworkOperationImpl final : public OpenNetworkOperation {
  public:
-  explicit Bg95AtModem(ObjProp prop, ModemAdapter& adapter, IPoller::ptr poller,
-                       ModemInit modem_init);
-  AE_OBJECT_REFLECT(AE_MMBRS(connect_vec_))
-
-  bool Init() override;
-  bool Start() override;
-  bool Stop() override;
-  ConnectionIndex OpenNetwork(Protocol protocol, std::string const& host,
-                              std::uint16_t port) override;
-  void CloseNetwork(ConnectionIndex connect_index) override;
-  void WritePacket(ConnectionIndex connect_index,
-                   DataBuffer const& data) override;
-  DataBuffer ReadPacket(ConnectionIndex connect_index,
-                        Duration timeout) override;
-  bool SetPowerSaveParam(PowerSaveParam const& psp) override;
-  bool PowerOff() override;
+  explicit OpenNetworkOperationImpl(AeContext const& ae_context,
+                                    Bg95AtModem& self, Protocol protocol,
+                                    std::string host, std::uint16_t port);
 
  private:
-  std::unique_ptr<ISerialPort> serial_;
-  std::unique_ptr<AtCommSupport> at_comm_support_;
-  std::vector<Bg95Connection> connect_vec_;
-  ModemAdapter* adapter_;
+  auto Pipeline();
+  void RunPipeline();
+
+  AeContext ae_context_;
+  Bg95AtModem* self_;
+  AtSupport& at_support_;
+  Protocol protocol_;
+  std::string host_;
+  std::uint16_t port_;
+  std::int32_t handle_{-1};
+};
+
+class CloseNetworkOperationImpl final : public ModemOperation {
+ public:
+  explicit CloseNetworkOperationImpl(AeContext const& ae_context,
+                                     Bg95AtModem& self,
+                                     ConnectionIndex connect_index);
+
+ private:
+  auto Pipeline();
+  void RunPipeline();
+
+  AeContext ae_context_;
+  Bg95AtModem* self_;
+  AtSupport& at_support_;
+  ConnectionIndex connect_index_;
+};
+
+class WriteOperationImpl final : public WriteOperation {
+ public:
+  explicit WriteOperationImpl(AeContext const& ae_context, Bg95AtModem& self,
+                              ConnectionIndex connect_index,
+                              std::span<std::uint8_t const> data);
+
+ private:
+  auto Pipeline();
+  void RunPipeline();
+
+  AeContext ae_context_;
+  Bg95AtModem* self_;
+  AtSupport& at_support_;
+  ConnectionIndex connect_index_;
+  std::span<std::uint8_t const> data_;
+};
+
+class ModemStartedAlreadyOperation final : public ModemOperation {
+ public:
+  explicit ModemStartedAlreadyOperation() { SetResult(Ok{kIgnore}); };
+};
+
+class ModemStartOperation final : public ModemOperation {
+ public:
+  explicit ModemStartOperation(AeContext const& ae_context,
+                               Bg95AtModem& self);
+
+ private:
+  auto SetBaudRate(kBaudRate const rate);
+  auto SetNetMode(kModemMode const modem_mode);
+  auto SetupNetwork(std::string const& operator_name,
+                    std::string const& operator_code,
+                    std::string const& apn_name, std::string const& apn_user,
+                    std::string const& apn_pass, kModemMode modem_mode,
+                    kAuthType auth_type);
+  auto CheckSimStatus();
+  auto SetupSim(std::uint16_t pin);
+  auto Pipeline();
+
+  void RunPipeline();
+
+  AeContext ae_context_;
+  Bg95AtModem* self_;
+  ModemInit modem_init_;
+  AtSupport& at_support_;
+};
+
+class ModemStoppedAlreadyOperation final : public ModemOperation {
+ public:
+  explicit ModemStoppedAlreadyOperation() { SetResult(Ok{kIgnore}); };
+};
+
+class ModemStopOperation final : public ModemOperation {
+ public:
+  explicit ModemStopOperation(AeContext const& ae_context,
+                               Bg95AtModem& self);
+
+ private:
+  auto Pipeline();
+  void RunPipeline();
+
+  AeContext ae_context_;
+  Bg95AtModem* self_;
+  AtSupport& at_support_;
+};
+
+// Power save parameters not implemented yet - returns success
+class ModemSetPowerSaveParamOperation final : public ModemOperation {
+ public:
+  explicit ModemSetPowerSaveParamOperation(AeContext const& /*ae_context*/,
+                                            Bg95AtModem& /*self*/,
+                                            ModemPowerSaveParam /*psp*/) {
+    SetResult(Ok{kIgnore});
+  }
+};
+
+class ModemPowerOffOperation final : public ModemOperation {
+ public:
+  explicit ModemPowerOffOperation(AeContext const& ae_context,
+                                   Bg95AtModem& self);
+
+ private:
+  auto Pipeline();
+  void RunPipeline();
+
+  AeContext ae_context_;
+  Bg95AtModem* self_;
+  AtSupport& at_support_;
+};
+}  // namespace bg95_modem_internal
+
+class Bg95AtModem final : public IModemDriver {
+  friend class bg95_modem_internal::OpenNetworkOperationImpl;
+  friend class bg95_modem_internal::CloseNetworkOperationImpl;
+  friend class bg95_modem_internal::WriteOperationImpl;
+  friend class bg95_modem_internal::ModemStartOperation;
+  friend class bg95_modem_internal::ModemStopOperation;
+  friend class bg95_modem_internal::ModemSetPowerSaveParamOperation;
+  friend class bg95_modem_internal::ModemPowerOffOperation;
 
   static constexpr std::uint16_t kModemMTU{1520};
 
-  kModemError CheckResponse(std::string response, std::uint32_t wait_time,
-                            std::string error_message);
-  kModemError SetBaudRate(kBaudRate rate);
-  kModemError CheckSimStatus();
-  kModemError SetupSim(const std::uint8_t pin[4]);
-  kModemError SetNetMode(kModemMode modem_mode);
-  kModemError SetupNetwork(std::string const& operator_name,
-                           std::string const& operator_code,
-                           std::string const& apn_name,
-                           std::string const& apn_user,
-                           std::string const& apn_pass, kModemMode modem_mode,
-                           kAuthType auth_type);
-  kModemError SetTxPower(kModemBand band, const float& power);
-  kModemError GetTxPower(kModemBand band, float& power);
-  kModemError DbmaToHex(kModemBand band, const float& power, std::string& hex);
-  kModemError HexToDbma(kModemBand band, float& power, const std::string& hex);
+ public:
+  explicit Bg95AtModem(AeContext const& ae_context,
+                       IPoller::ptr const& poller, ModemInit modem_init);
+
+  ModemOperation* Start() override;
+  ModemOperation* Stop() override;
+  OpenNetworkOperation* OpenNetwork(Protocol protocol, std::string const& host,
+                                     std::uint16_t port) override;
+  ModemOperation* CloseNetwork(ConnectionIndex connect_index) override;
+  WriteOperation* WritePacket(ConnectionIndex connect_index,
+                              std::span<std::uint8_t const> data) override;
+  DataEvent::Subscriber data_event() override;
+
+  ModemOperation* SetPowerSaveParam(ModemPowerSaveParam const& psp) override;
+  ModemOperation* PowerOff() override;
+
+ private:
+  void Init();
+  void SetupPoll();
+  void PollEvent(std::int32_t handle);
+
+  AeContext ae_context_;
+  ModemInit modem_init_;
+  std::unique_ptr<ISerialPort> serial_;
+  AtSupport at_support_;
+  ActionsQueue operation_queue_;
+  std::set<ConnectionIndex> connections_;
+  ConnectionIndex next_connection_index_{0};
+  DataEvent data_event_;
+  std::optional<AtListener> poll_listener_;
+
+  std::unique_ptr<ModemOperation> modem_start_operation_;
+  std::unique_ptr<ModemOperation> modem_stop_operation_;
+  std::unique_ptr<ModemOperation> modem_set_psp_operation_;
+  std::unique_ptr<ModemOperation> modem_poweroff_operation_;
+  ActionPool<AeContext, bg95_modem_internal::OpenNetworkOperationImpl, 10>
+      open_network_pool_;
+  ActionPool<AeContext, bg95_modem_internal::CloseNetworkOperationImpl, 10>
+      close_network_pool_;
+  ActionPool<AeContext, bg95_modem_internal::WriteOperationImpl, 10>
+      write_pool_;
+
+  bool initiated_;
+  bool started_;
+  int recv_in_queue_ = 0;
 };
 
 } /* namespace ae */
