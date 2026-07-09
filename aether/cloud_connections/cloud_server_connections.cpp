293218059ff92f3aad670edda0318231e9093d31
Kiryanov D V <dmitriyk@aethernet.io>
2026-07-09 17:02:56 +0300

    Fixing zero packet send.

--- aether/cloud_connections/cloud_server_connections.cpp
+++ aether/cloud_connections/cloud_server_connections.cpp
@@ -275,15 +275,21 @@ std::vector<CloudServerConnection*> CloudServerConnections::ServerCandidates() {
 
 WriteAction& CloudServerConnections::CallApi(ApiCall const& api_caller,
                                              RequestPolicy::Variant policy) {
+  AE_TELED_ERROR("[CALL-CHAIN] CloudServerConnections::CallApi selected={}",
+                 selected_servers_.size());
   std::vector<WriteAction*> swas;
   ForServers(
       [&](CloudServerConnection* sc) {
         auto* conn = sc->client_connection();
         assert((conn != nullptr) && "Client connection is null");
+        AE_TELED_ERROR("[CALL-CHAIN] CloudServerConnections::CallApi server={}",
+                       sc->server()->server_id);
         swas.emplace_back(&conn->AuthorizedApiCall(
             SubApi<AuthorizedApi>{[&](auto& api) { api_caller(api, sc); }}));
       },
       policy);
+  AE_TELED_ERROR("[CALL-CHAIN] CloudServerConnections::CallApi writes={}",
+                 swas.size());
 
   if (swas.empty()) {
     return EmptyWriteAction();
