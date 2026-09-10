006cf9eef545a1d7c84e3953f5fa1bcdc9498d2c
BartolomeyKant <notevill@gmail.com>
2026-09-10 17:11:02 +0500

    move all env to common

--- aether/common.h
+++ aether/common.h
@@ -19,9 +19,11 @@
 
 #include <cstdint>
 
-#include "aether/config.h"
+#if ESP_PLATFORM
+#  include "esp_attr.h"
+#endif
 
-namespace ae {
+#include "aether/config.h"
 
 #define _QUOTE(x) #x
 #define STR(x) _QUOTE(x)
@@ -37,18 +39,6 @@ namespace ae {
 #define AE_ESC(...) AE_ESC_(__VA_ARGS__)
 #define AE_ESC_(...) AE_VAN_##__VA_ARGS__
 #define AE_VAN_AE_ISH
-
-enum class CompressionMethod : std::uint8_t {
-  kNone = AE_NONE,
-#if AE_COMPRESSION_ZLIB == 1
-  kZlib = 1,
-#endif  // AE_COMPRESSION_ZLIB == 1
-#if AE_COMPRESSION_LZMA == 1
-  kLzma = 2,
-#endif  // AE_COMPRESSION_LZMA == 1
-};
-
-}  // namespace ae
 
 // default copy move constructors
 #define AE_CLASS_MOVE_(class_name, impl)          \
@@ -76,4 +66,65 @@ enum class CompressionMethod : std::uint8_t {
 #define AE_CLASS_NO_COPY_MOVE(class_name) \
   AE_CLASS_COPY_(class_name, delete)      \
   AE_CLASS_MOVE_(class_name, delete)
+
+#if defined(__BYTE_ORDER) && __BYTE_ORDER == __BIG_ENDIAN ||                 \
+    defined(__BIG_ENDIAN__) || defined(__ARMEB__) || defined(__THUMBEB__) || \
+    defined(__AARCH64EB__) || defined(_MIBSEB) || defined(__MIBSEB) ||       \
+    defined(__MIBSEB__) ||                                                   \
+    defined(Q_BYTE_ORDER) && Q_BYTE_ORDER == Q_BIG_ENDIAN
+#  define AE_ENDIANNESS AE_BIG_ENDIAN
+#elif defined(__BYTE_ORDER) && __BYTE_ORDER == __LITTLE_ENDIAN ||          \
+    defined(__LITTLE_ENDIAN__) || defined(__ARMEL__) ||                    \
+    defined(__THUMBEL__) || defined(__AARCH64EL__) || defined(__i386__) || \
+    defined(__amd64) || defined(__amd64__) || defined(_MIPSEL) ||          \
+    defined(__MIPSEL) || defined(__MIPSEL__) || defined(ESP_PLATFORM) ||   \
+    defined(Q_BYTE_ORDER) && Q_BYTE_ORDER == Q_LITTLE_ENDIAN
+#  define AE_ENDIANNESS AE_LITTLE_ENDIAN
+#else
+#  define AE_ENDIANNESS AE_LITTLE_ENDIAN
+// #error "Undefined endianness for the architecture"
+#endif
+
+namespace ae {
+template <typename T>
+T SwapToInet(const T& t) {
+#if AE_ENDIANNESS == AE_LITTLE_ENDIAN
+  union {
+    T t;
+    std::uint8_t t8[sizeof(T)];
+  } src, dest;
+  src.t = t;
+  for (size_t e = 0; e < sizeof(T); e++) {
+    dest.t8[e] = src.t8[sizeof(T) - e - 1];
+  }
+  return dest.t;
+#else
+  return t;
+#endif
+}
+
+template <typename T>
+T SwapToLittleEndian(const T& t) {
+#if AE_ENDIANNESS == AE_BIG_ENDIAN
+  union {
+    T t;
+    std::uint8_t t8[sizeof(T)];
+  } src, dest;
+  src.t = t;
+  for (size_t e = 0; e < sizeof(T); e++) {
+    dest.t8[e] = src.t8[sizeof(T) - e - 1];
+  }
+  return dest.t;
+#else
+  return t;
+#endif
+}
+}  // namespace ae
+
+#if ESP_PLATFORM
+#  define RTC_STORAGE_ATTR RTC_DATA_ATTR
+#else
+#  define RTC_STORAGE_ATTR
+#endif
+
 #endif  // AETHER_COMMON_H_
